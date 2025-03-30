2f410b76748a3f9e003fe3b4b9d09aff2f1875de
M-msdias <ddiasmarcela@gmail.com>
2025-03-30 16:18:43 -0300

    Update Questao4.java

--- Questao4.java
+++ Questao4.java
@@ -10,16 +10,17 @@ public class Questao4 {
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 System.out.print("Digite o valor para a posição [" + i + "][" + j + "]: ");
-                matriz[i][j] = scanner.nextInt();
+                matriz[i][j] = ler.nextInt();
             }
         }
-        System.out.println("\nMatriz 4x4 digitada:");
+        System.out.println("Matriz 4x4 digitada:");
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 System.out.print(matriz[i][j] + " ");
             }
             System.out.println();  
         }
+        
         int maiorValor = matriz[0][0];  
         int linhaMaior = 0;
         int colunaMaior = 0;
@@ -33,9 +34,9 @@ public class Questao4 {
                 }
             }
         }
-        System.out.println("\nO maior valor é " + maiorValor + ".");
+        System.out.println("O maior valor é " + maiorValor + ".");
         System.out.println("Sua localização é: Linha " + linhaMaior + ", Coluna " + colunaMaior + ".");
-       
+        
         ler.close();
     }
 }
