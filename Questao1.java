974774e9dc6e155acb3465f85b05286d689f6beb
M-msdias <ddiasmarcela@gmail.com>
2025-03-30 16:17:19 -0300

    Update Questao1.java

--- Questao1.java
+++ Questao1.java
@@ -10,17 +10,24 @@ public class Questao1 {
         System.out.println("Digite os valores para a matriz 4x4:");
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                System.out.print("Digite o valor para a posição [" + i + "][" + j + "]: ");
-                matriz[i][j] = sun.launcher.resources.nextInt();
-                
-    
+                System.out.print("Posição [" + (i+1) + "][" + (j+1) + "]: ");
+                matriz[i][j] = ler.nextInt();
+
                 if (matriz[i][j] > 10) {
                     contador++;
                 }
             }
         }
-        
-        System.out.println("\nA matriz possui " + contador + " valores maiores que 10.");
+
+        System.out.println("\nMatriz inserida:");
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.printf("%4d ", matriz[i][j]);
+            }
+            System.out.println();
+        }
+
+        System.out.println("A matriz possui " + contador + " valores maiores que 10.");
         
         ler.close();
     }
